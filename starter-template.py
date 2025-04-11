f95901d80e2c13acea7b613adbc68ed1fad5369d
Evans-mutuku <60472825+Evans-mutuku@users.noreply.github.com>
2025-04-11 16:47:23 +0300

    Create starter-template.py

---
+++ starter-template.py
@@ -0,0 +1,25 @@
+class Pet:
+    def __init__(self, name):
+        self.name = name
+        self.hunger = 5
+        self.energy = 5
+        self.happiness = 5
+        self.tricks = []
+
+    def eat(self):
+        # TODO
+
+    def sleep(self):
+        # TODO
+
+    def play(self):
+        # TODO
+
+    def train(self, trick):
+        # TODO
+
+    def show_tricks(self):
+        # TODO
+
+    def get_status(self):
+        # TODO
